a40fc576a18dd8fa886023db61f92fde8abcdfe2
spiral07 <33974537+spiral07@users.noreply.github.com>
2024-04-13 16:46:30 +0930

    commit

--- def factorial(n):.py
+++ def factorial(n):.py
@@ -4,3 +4,12 @@ def factorial(n):
     else:
         return n * factorial(n-1)
     
+
+import pandas as pd
+
+path = '/Users/nicks/Documents/GitHub/N-Shead/CSV4.csv'
+
+df = pd.read_csv(path)
+df
+text = df['TEXT'].iloc[0]
+print(text)
